ae1094a0b226f38ab4b96731bab5084622c42889
fbollon <fbollon@02912439-fd3b-0410-970e-99ecacbbc69c>
2007-10-12 12:57:59 +0000

    [fredistrano] divers avant release

--- app/config/config.dev.php
+++ app/config/config.dev.php
@@ -1,6 +1,6 @@
 <?php
 //HTTPS authentification
-define("_HTTPSENABLED",			false);				//enabled https
+define("_HTTPSENABLED",			true);				//enabled https
 
 //Facultatif utilisateur et mot de passe par défaut pour le serveur subversion
 define("_SVNUSER", "");
@@ -19,37 +19,20 @@ define("_FILEMODE", 654);
 // installation path de Fredistrano
 define("_FREDISTRANOPATH",dirname( dirname( dirname(__FILE__))));
 
+
 // Définition des dossiers utilisés par Fredistrano 
 define("_DEPLOYDIR", _FREDISTRANOPATH . DS . "files");
 define("_DEPLOYTMPDIR", _DEPLOYDIR . DS . "tmp");
 define("_DEPLOYBACKUPDIR", _DEPLOYDIR . DS . "backup");
 
+//durée maximum d'exécution 
+define("_TIMELIMITSVN", 10 * 60);
+define("_TIMELIMITRSYNC", 2 * 60);
 
-
-/**
- * 
- * SPECIFIQUE -sera supprimé dans une prochaine version 
- * 
- * */
-
-//serveur pour les web services
-define("_WEBSERVICESSERVER","hermes:50000");
-
-/*
- * méthode d'authentification
- * 1 = webservice + mysql
- * 0 = mysql
- *
- */
-define("_AUTHENTICATIONTYPE", "0");
-
-/*
- * annuaire utilisé par le webservice pour le webservice
- * 10 genesis
- * 11 yellowhat
- * 20 dnsan
-*/
-define("_DIRECTORYTYPE", "20");
-
+//SPECIFIQUE -sera supprimé dans une prochaine version
+define("_WEBSERVICESSERVER",	"hermes:50001"); 	// endpoint URL
+define("_AUTHENTICATIONTYPE",	"0");				// 0 = mysql / 1 = webservice + mysql
+define("_DIRECTORYTYPE",		"20");				// 10 = genesis / 11 = yellowhat / 20 = dnsan
+define("_WS_SSL_TRUSTEDCA_FILE","D:/Apache/Apache2/conf/ssl.crt/AdixenCA.crt");
 
 ?>
