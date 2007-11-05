c6d20b48d186fb3941d83cc4cf68b296ec712a89
fbollon <fbollon@02912439-fd3b-0410-970e-99ecacbbc69c>
2007-11-05 23:26:50 +0000

    [Fredistrano] - Issue # 1 - Internationalization (partial)

--- app/config/config.dev.php
+++ app/config/config.dev.php
@@ -3,15 +3,15 @@
 define("_HTTPSENABLED",	0);							//enabled https 0 = never, 1 = login, 2 = always
 
 //Comptes
-define("_SVNUSER", "");								//login subversion par défaut
-define("_SVNPASS", "");								//password subversion par défaut
+define("_SVNUSER", "");								//default subversion login
+define("_SVNPASS", "");								//default subversion password 
 
 // Délais
-define("_LOGSARCHIVEDATE",3 * 7 * 24 * 60 * 60);	//délai pour l'archive des logs 
-define("_TIMELIMIT_EXPORT", 10 * 60); 				//durée maximum d'exécution d un export subversion
-define("_TIMELIMIT_RSYNC", 1 * 60);					//durée maximum d'exécution d une synchro rsync
-define("_TIMELIMIT_FINALIZE", 5 * 60);				//durée maximum d'exécution apres le deploy
-define("_TIMELIMIT_INITIALIZE", 5 * 60);			//durée maximum d'exécution avant le deploy
+define("_LOGSARCHIVEDATE",3 * 7 * 24 * 60 * 60);	//time before archiving logs 
+define("_TIMELIMIT_EXPORT", 10 * 60); 				//max execution time for subversion export
+define("_TIMELIMIT_RSYNC", 1 * 60);					//max execution time for rsync
+define("_TIMELIMIT_FINALIZE", 5 * 60);				//max execution time after deploy
+define("_TIMELIMIT_INITIALIZE", 5 * 60);			//max execution time before deploy
 
 // Finalize options
 define("_RENAMEPRDFILE", true);						//rename ".prd." file
@@ -19,22 +19,24 @@ define("_CHANGEMODE", true);						//change file and directory mode
 define("_GIVEWRITEMODE", true);						//give write access on directories define in deploy.php in $writable
 
 // Permissions
-define("_DIRMODE", '750');							//permission sur les répertoires après le deploiement 750 en règle générale 
-define("_FILEMODE", '640');							//permission sur les fichiers après le deploiement 640 en règle générale
-define("_WRITEMODE", '777');						//droits d'écriture sur les dossiers définit dans deploy.php dans $writable après le deploiement 777 en règle générale
+define("_DIRMODE", '750');							//directories mode after deployment 
+define("_FILEMODE", '640');							//files mode after deployment 
+define("_WRITEMODE", '777');						//writable mode on directories defined in deploy.php in $writable after deployment 
 
 // Filesytem
-define("_FREDISTRANOPATH", dirname( dirname( dirname(__FILE__)))); 	//installation path de Fredistrano
-define("_DEPLOYDIR", _FREDISTRANOPATH . DS . "files");				//nécessaire à Fredistrano pour stocker les exports SVN et les backups
-define("_DEPLOYTMPDIR", _DEPLOYDIR . DS . "tmp");					//nécessaire à Fredistrano pour stocker les exports SVN
-define("_DEPLOYBACKUPDIR", _DEPLOYDIR . DS . "backup");				//nécessaire à Fredistrano pour stocker les backups
-define("_CYGWINROOT","/cygdrive/");									//uniquement pour cygwin sur les serveurs windows, en générale "/cygdrive/" ou "/"
+define("_FREDISTRANOPATH", dirname( dirname( dirname(__FILE__)))); 	//Fredistrano installation path  
+define("_DEPLOYDIR", _FREDISTRANOPATH . DS . "files");				//required by Fredistrano for SVN export and backup
+define("_DEPLOYTMPDIR", _DEPLOYDIR . DS . "tmp");					//required by Fredistrano for SVN export
+define("_DEPLOYBACKUPDIR", _DEPLOYDIR . DS . "backup");				//required by Fredistrano for backup
+define("_CYGWINROOT","/cygdrive/");									//only for cygwin windows server , example : "/cygdrive/" ou "/"
 
 // Language
 define('LANG_DEFAULT','en-US'); 					//available languages :  en-US, fr-FR
 require_once (APP . 'locale' . DS . LANG_DEFAULT . DS . 'LC_MESSAGES' . DS . 'default.php');
 
-//SPECIFIQUE -sera supprimé dans une prochaine version
+
+
+//SPECIFIC - Will be removed in a futur version
 define("_WEBSERVICESSERVER",	""); 				// endpoint URL
 define("_AUTHENTICATIONTYPE",	"0");				// 0 = mysql / 1 = webservice + mysql
 define("_DIRECTORYTYPE",		"20");				// 10 = genesis / 11 = yellowhat / 20 = dnsan
