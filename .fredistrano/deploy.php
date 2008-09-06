8dc45d5b84a2f6b103799df08a58b1bebbf387e6
fbollon <fbollon@02912439-fd3b-0410-970e-99ecacbbc69c>
2008-09-06 15:47:09 +0000

    code documentation of deploy.php

--- .fredistrano/deploy.php
+++ .fredistrano/deploy.php
@@ -1,29 +1,31 @@
 <?php
-// TODO Document code
  class DEPLOY_CONFIG {
-
+	
+	// Default deployment options for the current project
+	// these options could be modified during the deployment process in standard mode
+	// in fast mode these options will be used
  	var $options = array(
  		'export' 		=> array(),
  		'synchronize'	=> array(
- 		 	'runBeforeScript'		=> 	false,
- 			'backup'				=> 	false
+ 		 	'runBeforeScript'		=> 	false, 		//enable custom script before deployement 
+ 			'backup'				=> 	false 		//enable backup functionality
  		),
  		'finalize'		=> array(
-	 		'renamePrdFile' 		=> 	false,
-			'changeFileMode' 		=> 	false,
-			'giveWriteMode'			=> 	false,
- 			'runAfterScript'		=> 	false
+	 		'renamePrdFile' 		=> 	false,		//enable renaming .prd.xxx files
+			'changeFileMode' 		=> 	false,		//enable updating file mode
+			'giveWriteMode'			=> 	false,		//enable updating write mode on directories defined in $writable (in this file)
+ 			'runAfterScript'		=> 	false		//enable custom script at the end of the deployement process
  		)
  	);
  	
- 	// Scripts à exécuter avant/après que les fichiers ne soient modifiés
+ 	// path of yours custom scripts to execute at the beginning/end of the deployment process
+	// if yours scripts are located in a directory named ".fredistrano" at the root of your project enter only the name of your script to execute
  	var $scripts = array(
- 		'before' 	=>		'/path/to/file',
- 		'after' 	=>		'/path/to/file'
+ 		'before' 	=>		'/path/to/file', 
+ 		'after' 	=>		'/path/to/file' 
  	);
  	
-	// tableau permettant la création du fichier
-	// des répertoires et fichiers à exclure lors du dépoiement en production
+	// List of directories and files to exclude during the deployemnt process on the production server
 	var $exclude = array (
 		'app/tmp/logs/*',
 		'app/tmp/sessions/*',
@@ -35,8 +37,8 @@
 		'files/tmp/*'
 	);
 		
-	// répertoires sur lesquel un CHMOD 777 sera executé à la suite du déploiement
-	// les répertoires de log, de cache, les répertoires d'upload, etc ...
+	// Directories list on which the write permission will applied during the finalization step of the deployment process	
+	// log, cache, upload directories, etc...
 	var $writable = array (
 		'app/tmp',
 		'files/backup',
